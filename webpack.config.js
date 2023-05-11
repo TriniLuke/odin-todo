b3f931bc92ab6e26bce11294c7eb3e560b2e65a3
Luke Liverpool <lliverpool86@gmail.com>
2023-05-11 16:19:30 -0400

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,19 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ['@babel/plugin-transform-runtime'],
+          }
+        }
+      },
     ],
   },
 };
